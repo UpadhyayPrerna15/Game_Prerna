ab72e5459e3b2d6b53285b8623c5944bd43d1a07
Upadhyay <prerna.upadhyay@bp.com>
2025-11-19 12:22:51 +0530

    final changes

--- dodger-master/game.test.js
+++ dodger-master/game.test.js
@@ -1,23 +1,23 @@
 const { test } = require('@jest/globals');
-const { checkCollisionBetween, isWithinBounds } = require ('./gameLogic');
+const { checkCollisionBetween, isWithinBounds } = require('./gameLogic');
 
-test ('detects collision between two objects', () => {
-    const player = { x:100, y:100, width: 50, height: 50 };
-    const mosquito = { x:120, y:120, width: 30, height: 30 };
+test('detects collision between two objects', () => {
+    const player = { x: 100, y: 100, width: 50, height: 50 };
+    const mosquito = { x: 120, y: 120, width: 30, height: 30 };
     
     const result = checkCollisionBetween(player, mosquito);
     expect(result).toBe(true);
 });
 
-test ('detects no collision between two objects', () => {
-    const player = { x:100, y:100, width: 50, height: 50 };
-    const mosquito = { x:200, y:200, width: 30, height: 30 };
+test('detects no collision between two objects', () => {
+    const player = { x: 100, y: 100, width: 50, height: 50 };
+    const mosquito = { x: 200, y: 200, width: 30, height: 30 };
     
     const result = checkCollisionBetween(player, mosquito);
     expect(result).toBe(false);
 });
 
-test ( 'checks if an object is within bounds', () => {
+test('checks if an object is within bounds', () => {
     const x = 100;
     const width = 50;
     const canvasWidth = 500;
@@ -26,7 +26,7 @@ test ( 'checks if an object is within bounds', () => {
     expect(result).toBe(true);
 });
 
-test ( 'checks if an object is out of bounds', () => {
+test('checks if an object is out of bounds', () => {
     const x = 480;
     const width = 50;
     const canvasWidth = 500;
